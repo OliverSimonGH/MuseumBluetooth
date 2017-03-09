1af64536511c7182bbcf9e882e2f2ebeb0aff0b4
Ciara Langton <langtonc@cardiff.ac.uk>
2017-03-09 11:20:29 +0000

    Set up of nearby devices activity and xml, added a text view to the xml to add the list of nearby devices to

---
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/NearbyDevices.java
@@ -0,0 +1,13 @@
+package com.example.team11.museumaudiotrailsteam11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NearbyDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_devices);
+    }
+}
