7584744bf617547423438d272fe382751eb67043
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-12 12:45:57 +0000

    Commented code.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBSQLiteHelper.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBSQLiteHelper.java
@@ -9,18 +9,18 @@ import android.icu.text.MessageFormat;
 /**
  * Created by c1630186 on 12/03/2017.
  */
-public class DBSQLiteHelper extends SQLiteOpenHelper{
-    public static final String TABLE_NAME = "ListExhibits";
-    public static final String COL_ID = "Exhibit_ID";
-    public static final String COl_Item = "Exhibit_Txt";
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "Exhibits.db";
+public class DBSQLiteHelper extends SQLiteOpenHelper{   // Extending the Android SQLDB class that offers all the SQL executable commands
+    public static final String TABLE_NAME = "ListExhibits"; // First Table which is a List of Exhibits. Note this can change if needed.
+    public static final String COL_ID = "Exhibit_ID";   // Name of First Column is Exhibit ID. Its a STRING as "Exhibit_ID" is a String
+    public static final String COl_Item = "Exhibit_Txt";    // THe Column that'll contain the name of the Exhibit
+    public static final int DATABASE_VERSION = 1;   // This is the first version of our DB
+    public static final String DATABASE_NAME = "Exhibits.db";   // Name of the Database
 
-    public DBSQLiteHelper(Context context){ super(context, DATABASE_NAME, null, DATABASE_VERSION);}
+    public DBSQLiteHelper(Context context){ super(context, DATABASE_NAME, null, DATABASE_VERSION);} // Constructor for the DB
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        String create = MessageFormat.format("CREATE TABLE IF NOT EXISTS {0}({1} INTEGER PRIMARY KEY AUTOINCREMENT, {2} VARCHAR);");
-        db.execSQL(create);
+    public void onCreate(SQLiteDatabase db) {   // OnCreate Method - This is what the Db does when its first created
+        String create = MessageFormat.format("CREATE TABLE IF NOT EXISTS {0}({1} INTEGER PRIMARY KEY AUTOINCREMENT, {2} VARCHAR);"); // Made a formatted string with some executable SQL Code
+        db.execSQL(create); // Executes the SQL Code above.
     }
 
     @Override
