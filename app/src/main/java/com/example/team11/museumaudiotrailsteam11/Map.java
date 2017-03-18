97ad24d20d62385a340c7a53eb1e563f2461a2e9
Ciara Langton <langtonc@cardiff.ac.uk>
2017-03-18 17:23:44 +0000

    Search bar working, searches through a listview of museum names

---
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/Map.java
@@ -0,0 +1,15 @@
+package com.example.team11.museumaudiotrailsteam11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        
+    }
+}
