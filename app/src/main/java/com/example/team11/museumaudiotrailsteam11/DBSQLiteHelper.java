51693f82124728482d1e1cb53b5d9b3261a7fc4f
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-18 15:52:08 +0000

    Fixed that formatting error. Will merge o master.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBSQLiteHelper.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBSQLiteHelper.java
@@ -4,7 +4,7 @@ import android.app.Notification;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.icu.text.MessageFormat;
+import java.text.MessageFormat;
 
 /**
  * Created by c1630186 on 12/03/2017.
@@ -19,7 +19,7 @@ public class DBSQLiteHelper extends SQLiteOpenHelper{   // Extending the Android
     public DBSQLiteHelper(Context context){ super(context, DATABASE_NAME, null, DATABASE_VERSION);} // Constructor for the DB
     @Override
     public void onCreate(SQLiteDatabase db) {   // OnCreate Method - This is what the Db does when its first created
-        String create = MessageFormat.format("CREATE TABLE IF NOT EXISTS {0}({1} INTEGER PRIMARY KEY AUTOINCREMENT, {2} VARCHAR);"); // Made a formatted string with some executable SQL Code
+        String create = MessageFormat.format("CREATE TABLE IF NOT EXISTS {0}({1} INTEGER PRIMARY KEY AUTOINCREMENT, {2} VARCHAR);", TABLE_NAME, COL_ID, COl_Item); // Made a formatted string with some executable SQL Code
         db.execSQL(create); // Executes the SQL Code above.
     }
 
