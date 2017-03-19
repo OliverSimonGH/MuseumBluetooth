e6d497d21ec0095eeda0c1a47d8fae4afd7e7d9a
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-19 14:31:58 +0000

    Added methods inside the DBConnector that allows you to add exhibits to the DB and Remove them.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBConnector.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/DBConnector.java
@@ -1,8 +1,37 @@
 package com.example.team11.museumaudiotrailsteam11;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by c1630186 on 19/03/2017.
  */
 public class DBConnector {
+    DBSQLiteHelper openHelper;
+    String TABLE_NAME = "ListExhibits";
+    String COL_ID = "Exhibit_ID";
+    String COl_Exhibit = "Exhibit_Txt";
 
+    public DBConnector(Context ctx){
+        openHelper = new DBSQLiteHelper(ctx);
+    }
+
+    public long addExhibit(String exhibit){
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("Exhibit_Txt", exhibit);
+        long rowId = db.insert(DBSQLiteHelper.TABLE_NAME, null, cv);
+        db.close();
+        return rowId;
+    }
+
+    public int removeItem(String id){
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        String exhibitLocation = DBSQLiteHelper.COL_ID + " = ?";
+        String[] args = {id};
+        int deleted = db.delete(DBSQLiteHelper.TABLE_NAME, exhibitLocation, args);
+        return deleted;
+    }
+    
 }
