725420e8bf9590a2fd5b8cd80ba20bbcc69045ef
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-20 20:31:51 +0000

    Created exhibitAdapter which could be updated in the future. Exhibits table should be finished and you should be able to add Exhibits to the this table. Note that this table is generalised in the terms that it is a list of all the Exhibits used in every museum.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/exhibitAdapter.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/exhibitAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,25 +21,33 @@ public class exhibitAdapter extends BaseAdapter implements Filterable {
     LayoutInflater myInflater;
     DBConnector exhibitConnector;
 
-    public exhibitAdapter(Context context){
-        exhibitConnector = new DBConnector(context);
+    public exhibitAdapter(Context context){ // Constructor for the exhibitAdapter
+        exhibitConnector = new DBConnector(context);   // Declaring our DBConstructor and setting it in the context of the exhibit table
         Map<Long, String > exhibits = exhibitConnector.getExhibits(null);
+        this.dataSource = new ArrayList<>();
+        this.dataSource.addAll(exhibits.values());
+        myInflater = LayoutInflater.from(context);
+    }
 
+    public void addExhibit(String exhibit){
+        Long i = this.exhibitConnector.addExhibit(exhibit);
+        this.dataSource.add(exhibit);
+        notifyDataSetChanged();
     }
 
 
     @Override
     public int getCount() {
-        return 0;
+        return dataSource.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int i) {
+        return dataSource.get(i);
     }
 
     @Override
-    public long getItemId(int position) {
+    public long getItemId(int i) {
         return 0;
     }
 
@@ -47,8 +56,44 @@ public class exhibitAdapter extends BaseAdapter implements Filterable {
         return null;
     }
 
+    public void remove(int i){
+        exhibitConnector.removeExhibit(String.valueOf(i));
+        this.dataSource.remove(i);
+        notifyDataSetChanged();
+    }
+
+
     @Override
     public Filter getFilter() {
-        return null;
+        Filter exhibitFilter = new Filter(){
+            @Override
+            protected FilterResults performFiltering(CharSequence filterTerm){
+                FilterResults filterExhibits = new FilterResults();
+                List<String> filteredExhibits = new ArrayList<>();
+
+                if(originalDataSource == null) {
+                    originalDataSource = new ArrayList<>(dataSource);
+                }
+
+                if(filterTerm == null || filterTerm.length() == 0) {
+                    filterExhibits.count = originalDataSource.size();
+                    filterExhibits.values = originalDataSource;
+                }else{
+
+                }
+                return filterExhibits;
+
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                dataSource = (List<String>) filterResults.values;
+                notifyDataSetChanged();
+            }
+
+
+        };
+        return exhibitFilter;
+
     }
 }
