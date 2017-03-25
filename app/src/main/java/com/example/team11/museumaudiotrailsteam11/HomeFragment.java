c05013e644db875c0e9f264f67f0931181075ea0
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-25 13:55:00 +0000

    Added the code in the fragment classes to switch between each of the fragments using the bottom navigation view. UI is ready to be hooked up to back end. Please let me know if you require me to add more pages to the UI.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/HomeFragment.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/HomeFragment.java
@@ -1,7 +1,38 @@
 package com.example.team11.museumaudiotrailsteam11;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 /**
  * Created by c1630186 on 25/03/2017.
  */
-public class HomeFragment {
+public class HomeFragment extends Fragment {
+    Button homeCheckButton;
+
+    public static HomeFragment newInstance(){
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.home_fragment_layout, container, false);
+        homeCheckButton = (Button)view.findViewById(R.id.btn_home_check);
+        homeCheckButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "This Button works!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
 }
