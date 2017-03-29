067cc9b5ae5dda42b59b98a9d1b1af22bfb53ef0
Morgan Jones <JonesM125@cardiff.ac.uk>
2017-03-29 18:13:39 +0100

    Figuting out way to switch from a fragment to an activity since implementing Beacon classes will be a pain.

--- app/src/main/java/com/example/team11/museumaudiotrailsteam11/HomeFragment.java
+++ app/src/main/java/com/example/team11/museumaudiotrailsteam11/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.team11.museumaudiotrailsteam11;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,11 +11,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.team11.museumaudiotrailsteam11.BeaconHistory.BeaconHistory;
+import com.example.team11.museumaudiotrailsteam11.BeaconScan.BeaconsListScreen;
+
 /**
  * Created by c1630186 on 25/03/2017.
  */
 public class HomeFragment extends Fragment {
     ImageView startPic;
+    Button scanningBtn;
+    Button accessHistorybtn;
 
     public static HomeFragment newInstance(){
         HomeFragment fragment = new HomeFragment();
@@ -29,6 +35,27 @@ public class HomeFragment extends Fragment {
 
         startPic = (ImageView)view.findViewById(R.id.imageHome);
         startPic.setImageResource(R.drawable.lates_544x216);
+        scanningBtn = (Button)view.findViewById(R.id.scanBtn);
+        accessHistorybtn = (Button)view.findViewById(R.id.btnHistory);
+
+        scanningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), BeaconsListScreen.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+        accessHistorybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), BeaconHistory.class);
+                getActivity().startActivity(intent);
+
+            }
+        });
 
         return view;
     }
